ea8c2decf8cc2189b5b2e242c3dc2461f380c966
Denis Oleshkevich <doleshkevich@neoflex.ru>
2013-07-30 18:23:27 +0400

    add apex to script

--- compile_copy.cmd
+++ compile_copy.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-c:/soft/lessc/lessc.cmd cognos.less cognos.css --compress & copy /Y  cognos.css "\\cognosdemo\NR classic\_common\cognos.css"
+c:/soft/lessc/lessc.cmd cognos.less cognos.css --compress & copy /Y  cognos.css "\\cognosdemo\NR classic\_common\cognos.css" & c:/soft/lessc/lessc.cmd apex.less apex.css --compress & copy /Y  apex.css "\\cognosdemo\NR classic\_common\apex.css"
