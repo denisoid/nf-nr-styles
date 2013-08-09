bd76b2df259a20af9e3120b131ec8d8de559500e
Denis Oleshkevich <doleshkevich@neoflex.ru>
2013-08-09 11:37:34 +0400

    styles + images

--- compile_copy.cmd
+++ compile_copy.cmd
@@ -1,3 +1,14 @@
 @echo off
 
-c:/soft/lessc/lessc.cmd cognos.less cognos.css --compress & copy /Y  cognos.css "\\cognosdemo\NR classic\_common\cognos.css" & c:/soft/lessc/lessc.cmd apex.less apex.css --compress & copy /Y  apex.css "\\cognosdemo\NR classic\_common\apex.css"
+call c:/soft/lessc/lessc.cmd cognos.less cognos.css --compress > NUL
+
+copy /Y  cognos.css "\\cognosdemo\NR classic\_common\cognos.css"
+
+
+call c:/soft/lessc/lessc.cmd apex.less apex.css --compress
+
+
+copy /Y  apex.css "\\cognosdemo\NR classic\_common\apex.css"
+
+
+copy /Y  i\ "\\cognosdemo\NR classic\_common\i\"
